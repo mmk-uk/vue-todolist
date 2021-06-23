efe70c1bf217a26f9ce86240965a3b27c854d3eb
村中勇輝 <mmk-uki@MacBook-Pro-3.local>
2021-06-24 00:31:04 +0900

    バーの表示

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import TodoList from '../views/TodoList.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'TodoList',
+    component: TodoList
   },
   {
     path: '/about',
---
+++ src/views/TodoList.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-container>
+      リスト
+
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'TodoList',
+}
+</script>
